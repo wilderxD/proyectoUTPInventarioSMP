afdd78bf88cc962649f6959d83cbb2e1b5a5ee04
wilderxD <wilder1093@gmail.com>
2025-07-04 17:44:47 -0500

    new commit 04/07

---
+++ src/main/java/com/utp/gp/inventarioSMP/dao/RolDao.java
@@ -0,0 +1,9 @@
+package com.utp.gp.inventarioSMP.dao;
+
+import com.utp.gp.inventarioSMP.entidades.Rol;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long>{
+    List<Rol> findAllByOrderByDescripcionRolAsc();
+}
